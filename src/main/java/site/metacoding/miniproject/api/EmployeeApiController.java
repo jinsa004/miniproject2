7bb769128a623ae61f5f40a9a276d6488471a130
jinsa004 <112357294+jinsa004@users.noreply.github.com>
2022-11-03 21:21:06 +0900

    구독기업 공고목록보기 기능 완료 (#26)

--- src/main/java/site/metacoding/miniproject/api/EmployeeApiController.java
+++ src/main/java/site/metacoding/miniproject/api/EmployeeApiController.java
@@ -1,25 +1,15 @@
 package site.metacoding.miniproject.api;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import lombok.RequiredArgsConstructor;
 import site.metacoding.miniproject.dto.ResponseDto;
-import site.metacoding.miniproject.dto.subscribe.SubscribeReqDto;
-import site.metacoding.miniproject.dto.subscribe.SubscribeRespDto;
-import site.metacoding.miniproject.dto.subscribe.SubscribeReqDto.SubscribeSaveReqDto;
-import site.metacoding.miniproject.dto.subscribe.SubscribeRespDto.SubscribeSaveRespDto;
 import site.metacoding.miniproject.dto.employee.EmpReqDto.EmpJoinReqDto;
-import site.metacoding.miniproject.dto.employee.EmpReqDto.EmpLoginReqDto;
 import site.metacoding.miniproject.dto.employee.EmpRespDto.EmpJoinRespDto;
-import site.metacoding.miniproject.dto.employee.EmpSessionUser;
 import site.metacoding.miniproject.service.EmployeeService;
 import site.metacoding.miniproject.service.IntroService;
 
