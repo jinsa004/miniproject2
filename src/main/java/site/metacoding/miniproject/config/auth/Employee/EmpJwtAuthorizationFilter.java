6e5abdffc81034a968d7954e598a3fd2c32d7370
mb6282 <marbea6282@gmail.com>
2022-11-04 15:44:41 +0900

    reqest 헤더 Authorization에 jwtToken이 있는지 확인

--- src/main/java/site/metacoding/miniproject/config/auth/Employee/EmpJwtAuthorizationFilter.java
+++ src/main/java/site/metacoding/miniproject/config/auth/Employee/EmpJwtAuthorizationFilter.java
@@ -20,6 +20,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import site.metacoding.miniproject.dto.ResponseDto;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -27,7 +28,27 @@ public class EmpJwtAuthorizationFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
 
+        // 헤더 Authorization 키값에 Bearer로 적힌 값이 있는지 체크
+        String jwtToken = req.getHeader("Authorization");
+        log.debug("디버그 : " + jwtToken);
+        if (jwtToken == null) {
+            filterResponse("JWT토큰이 없어서 인가할 수 없습니다", resp);
+            return;
+        }
+    }
+
+    private void filterResponse(String msg, HttpServletResponse resp) throws IOException, JsonProcessingException {
+        resp.setContentType("application/json; charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        resp.setStatus(400);
+        ResponseDto<?> responseDto = new ResponseDto<>(-1, msg, null);
+        ObjectMapper om = new ObjectMapper();
+        String body = om.writeValueAsString(responseDto);
+        out.println(body);
+        out.flush();
     }
 
 }
