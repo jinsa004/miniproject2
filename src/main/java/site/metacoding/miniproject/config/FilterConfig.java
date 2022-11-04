105ff80b1e4f17b3696ebc6cf993218488f0a606
mb6282 <marbea6282@gmail.com>
2022-11-04 15:40:04 +0900

    EmpJwtAuthorizationFilter Bean 띄우기

--- src/main/java/site/metacoding/miniproject/config/FilterConfig.java
+++ src/main/java/site/metacoding/miniproject/config/FilterConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import site.metacoding.miniproject.config.auth.Employee.EmpJwtAuthenticationFilter;
+import site.metacoding.miniproject.config.auth.Employee.EmpJwtAuthorizationFilter;
 import site.metacoding.miniproject.domain.employee.EmployeeDao;
 
 @Slf4j
@@ -25,16 +26,14 @@ public class FilterConfig {
         return bean;
     }
 
-    // @Bean
-    // public FilterRegistrationBean<JwtAuthorizationFilter>
-    // jwtAuthorizationFilterRegister() {// IoC등록 서버실행시
-    // log.debug("디버그 : 인가 필터 등록");
-    // FilterRegistrationBean<JwtAuthorizationFilter> bean = new
-    // FilterRegistrationBean<>(
-    // new JwtAuthorizationFilter());
-    // bean.addUrlPatterns("/s/*"); // 원래 주소창 뒤엔 **로 모든 걸 포함할 수 있지만 여기서는 * 하나로 예외.
-    // bean.setOrder(2);// 낮은 순서대로 실행
-    // return bean;
-    // }
+    @Bean
+    public FilterRegistrationBean<EmpJwtAuthorizationFilter> jwtAuthorizationFilterRegister() {// IoC등록 서버실행시
+        log.debug("디버그 : 인가 필터 등록");
+        FilterRegistrationBean<EmpJwtAuthorizationFilter> bean = new FilterRegistrationBean<>(
+                new EmpJwtAuthorizationFilter());
+        bean.addUrlPatterns("/s/*"); // 원래 주소창 뒤엔 **로 모든 걸 포함할 수 있지만 여기서는 * 하나로 예외.
+        bean.setOrder(2);
+        return bean;
+    }
 
 }
---
+++ src/main/java/site/metacoding/miniproject/config/auth/Employee/EmpJwtAuthorizationFilter.java
@@ -0,0 +1,33 @@
+package site.metacoding.miniproject.config.auth.Employee;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class EmpJwtAuthorizationFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+    }
+
+}
