17141fecc800e9ff0c69a7da6ee32698a6047b44
mb6282 <marbea6282@gmail.com>
2022-11-04 15:36:20 +0900

    JWT토큰 생성 및 응답 완료

--- src/main/java/site/metacoding/miniproject/config/auth/Employee/EmpJwtAuthenticationFilter.java
+++ src/main/java/site/metacoding/miniproject/config/auth/Employee/EmpJwtAuthenticationFilter.java
@@ -23,12 +23,14 @@ import lombok.extern.slf4j.Slf4j;
 import site.metacoding.miniproject.domain.employee.Employee;
 import site.metacoding.miniproject.domain.employee.EmployeeDao;
 import site.metacoding.miniproject.dto.ResponseDto;
+import site.metacoding.miniproject.dto.employee.EmpSessionUser;
 import site.metacoding.miniproject.dto.employee.EmpReqDto.EmpLoginReqDto;
 import site.metacoding.miniproject.util.SHA256;
 
 @Slf4j
 @RequiredArgsConstructor
 public class EmpJwtAuthenticationFilter implements Filter {
+
     private final EmployeeDao employeeDao; // DI (FilterConfig 주입받음)
 
     @Override
@@ -65,6 +67,21 @@ public class EmpJwtAuthenticationFilter implements Filter {
             filterResponse("패스워드가 틀렸습니다.", resp);
             return;
         }
+
+        // JWT토큰 생성 1초 = 1/1000
+        Date expire = new Date(System.currentTimeMillis() + (1000 * 60 * 60));
+
+        String jwtToken = JWT.create()
+                .withSubject("메타코딩")
+                .withExpiresAt(expire)
+                .withClaim("id", employeePS.getEmployeeId())
+                .withClaim("username", employeePS.getEmployeeUsername())
+                .sign(Algorithm.HMAC512("취직"));
+        log.debug("디버그 : " + jwtToken);
+
+        // JWT토큰 응답
+        filterJwtResponse(jwtToken, employeePS, resp);
+
     }
 
     private void filterResponse(String msg, HttpServletResponse resp) throws IOException, JsonProcessingException {
@@ -72,6 +89,19 @@ public class EmpJwtAuthenticationFilter implements Filter {
         PrintWriter out = resp.getWriter();
         resp.setStatus(400);
         ResponseDto<?> responseDto = new ResponseDto<>(-1, msg, null);
+        ObjectMapper om = new ObjectMapper();
+        String body = om.writeValueAsString(responseDto);
+        out.println(body);
+        out.flush();
+    }
+
+    private void filterJwtResponse(String token, Employee employeePS, HttpServletResponse resp)
+            throws IOException, JsonProcessingException {
+        resp.setContentType("application/json; charset=utf-8");
+        resp.setHeader("Authorization", "Bearer " + token);
+        PrintWriter out = resp.getWriter();
+        resp.setStatus(200);
+        ResponseDto<?> responseDto = new ResponseDto<>(1, "로그인 성공", new EmpSessionUser(employeePS));
         ObjectMapper om = new ObjectMapper();
         String body = om.writeValueAsString(responseDto);
         out.println(body);
